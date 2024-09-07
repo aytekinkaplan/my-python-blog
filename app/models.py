a297abfb350d13e8d8bd6235f09913d7774acc56
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-09-07 11:14:57 +0300

    Up

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    code = db.Column(db.Text)
+    output = db.Column(db.Text)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,9 @@ from app import app, db
 from app.models import BlogPost
 from config import Config
 
-
-
 @app.route('/')
-def index():
-    page = request.args.get('page', 1, type=int)
+@app.route('/page/<int:page>')
+def index(page=1):
     posts = BlogPost.query.order_by(BlogPost.date_posted.desc()).paginate(
         page=page, 
         per_page=Config.POSTS_PER_PAGE,
@@ -15,11 +13,11 @@ def index():
     )
     return render_template('index.html', posts=posts)
 
-
 @app.route('/post/<int:post_id>')
 def post(post_id):
     post = BlogPost.query.get_or_404(post_id)
     return render_template('blog_post.html', post=post)
+
 @app.route('/categories')
 def categories():
     categories = ['Python Basics', 'Web Development', 'Data Science', 'Machine Learning', 'Automation']
